f5c6d39863dcfb77fc34f9264cb69646c3b9d899
Wall E. Deau <walldeau@gmail.com>
2014-02-01 00:43:48 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waldoandjoyce::Application.config.secret_token = 'fda053a5a71a3d75f63fc27cd9b39dbf811c0645a6e0c0b754243f7cb0077fa0cda957bc035f0eb9a1349e73bba12fb62fab7759dcb6193da062d917fc20dd4f'
